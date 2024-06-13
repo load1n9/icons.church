8eb9bdaa6ff462339017bfe5fdf9d78102c3a81e
Dean Srebnik <49134864+load1n9@users.noreply.github.com>
2024-06-12 23:56:44 -0400

    feat: init

--- routes/[pack]/[icon].ts
+++ routes/[pack]/[icon].ts
@@ -2,23 +2,21 @@ import { FreshContext } from "$fresh/server.ts";
 
 export const handler = async (
   _req: Request,
-  { params: { pack, icon } }: FreshContext,
+  ctx: FreshContext,
 ): Promise<Response> => {
-  const url = `https://api.iconify.design/${pack}/${icon}.svg`;
+  const width = ctx.url.searchParams.get("width") || "16";
+  const height = ctx.url.searchParams.get("height") || "16";
+  const color = ctx.url.searchParams.get("color") || "currentColor";
+  const box = ctx.url.searchParams.get("box") || "false";
+  const url =
+    `https://api.iconify.design/${ctx.params.pack}/${ctx.params.icon}.svg?width=${width}&height=${height}&color=${color}&box=${box}`;
   try {
     const response = await fetch(url);
     const body = await response.text();
 
     return new Response(
-      `export default function GeneratedIcon({
-    width=16,
-    height=16, 
-    color = "currentColor",
-    box = false,
-}) {
-    return (
-    ${body}
-    )
+      `export default function GeneratedIcon(props) {
+    return <svg dangerouslySetInnerHTML={{ __html: \`${body}\` }} {...props}></svg>;
 }`,
       {
         headers: {
